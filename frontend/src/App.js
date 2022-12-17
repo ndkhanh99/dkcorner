e8ce99b43ef89986f9fa2f49b3d01393ee2b22a4
duykhanh <ndkhanh0901@gmail.com>
2022-12-17 15:32:42 +0700

    design content-cards-footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Cards from "./components/Cards";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Newmessage from "./components/Newmessage";
 import Product from "./components/Product";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Content />
       <Product />
+      <Newmessage />
+      <Cards />
+      <Footer />
     </div>
   );
 }
