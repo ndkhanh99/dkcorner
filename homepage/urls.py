0baf15ba58ca5303fb137d99b00ad73d7b33b51a
duykhanh <ndkhanh0901@gmail.com>
2023-01-02 17:15:49 +0700

    test

--- homepage/urls.py
+++ homepage/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('product', views.index, name='product'),
     path('about', views.index, name='about'),
     path('contact', views.index, name='contact'),
-    path('test', MenuListView.as_view()),
-
+    path('test', MenuListView.as_view(), name='test'),
+    path('test-ip', views.test, name='test-ip'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics 
 from .models import MenuItems
 from .serializers import MenuItemsSerializers
+from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
-
 class MenuListView(generics.CreateAPIView):
     queryset = MenuItems.objects.all()
     serializer_class = MenuItemsSerializers
+    
+def test(request):
+    return HttpResponse('Test sucessfully!')
+    
+    
 
     
